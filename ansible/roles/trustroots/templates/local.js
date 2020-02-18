f101e8ed316b69ab12accf612c187db827f6118e
Nick Sellen <git@nicksellen.co.uk>
2020-02-18 11:45:44 +0100

    Update deployment config

--- ansible/roles/trustroots/templates/local.js
+++ ansible/roles/trustroots/templates/local.js
@@ -10,4 +10,21 @@ module.exports = {
       auth: false,
     },
   },
+  sentry: {
+    enabled: true,
+    options: {
+      dsn: 'https://d4b8faee4ba345d09c8fb9fe0cc377f1@sentry.io/2578533',
+    },
+  },
+  db: {
+    uri: 'mongodb://localhost/trustroots',
+    options: {
+      auth: {
+        authMechanism: '',
+      },
+    },
+    debug: false,
+    autoIndex: true,
+    checkCompatibility: false,
+  },
 }
